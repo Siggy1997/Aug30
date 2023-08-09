140021f7dbaad2bfc7460c0e9ccc846dfff1c95b
siggy <siggy@BOOK-8V7IG0LIJL>
2023-08-09 18:21:49 +0900

    SpringBoot

---
+++ jsp/src/main/java/com/siggy/web/BoardDAO.java
@@ -0,0 +1,11 @@
+package com.siggy.web;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface BoardDAO {
+	
+	public Integer toSize();
+}
---
+++ jsp/src/main/java/com/siggy/web/IndexController.java
@@ -0,0 +1,30 @@
+package com.siggy.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class IndexController {
+	@Autowired
+	private BoardService boardService;
+	
+	
+	@GetMapping(value= {"/","/index"})
+	public String index() {
+		return "index";
+	}
+	
+	
+	@GetMapping("/board")
+	public ModelAndView board() {
+		ModelAndView mv = new ModelAndView("board");
+		
+		//전체 글 수 뽑아보기
+		int toSize = boardService.toSize();
+		mv.addObject("toSize", toSize);
+		return mv;
+	}
+}
